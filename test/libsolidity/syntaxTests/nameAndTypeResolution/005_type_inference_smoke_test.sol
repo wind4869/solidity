85aa0d81d4df38bece66c1874feecdfb53413e2f
Christian Parpart <christian@parpart.family>
2018-06-12 12:21:06 +0200

    remove some tests that aren't useful anymore (thx @axic)

--- test/libsolidity/syntaxTests/nameAndTypeResolution/005_type_inference_smoke_test.sol
+++
@@ -1,7 +0,0 @@
-contract test {
-    function f(uint256 arg1, uint32 arg2) public returns (bool ret) {
-        bool x = arg1 + arg2 == 8; ret = x;
-    }
-}
-// ----
-// Warning: (20-135): Function state mutability can be restricted to pure
--- test/libsolidity/syntaxTests/nameAndTypeResolution/012_type_inference_explicit_conversion.sol
+++
@@ -1,5 +0,0 @@
-contract test {
-    function f() public returns (int256 r) { int256 x = int256(uint32(2)); return x; }
-}
-// ----
-// Warning: (20-102): Function state mutability can be restricted to pure
